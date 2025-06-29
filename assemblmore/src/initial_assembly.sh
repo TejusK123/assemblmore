30946a00fe35df986dbea24187f159f63c35a3c9
TejusK123 <tkotikalapudi@gmail.com>
2025-06-29 00:46:09 -0600

    removed cli spam

--- assemblmore/src/initial_assembly.sh
+++ assemblmore/src/initial_assembly.sh
@@ -26,7 +26,7 @@ python "$SCRIPT_DIR/contig_placements.py" "$MAPPED_CONTIGS" "$READS"
 
 while IFS=$'\t' read -r id _ strand _ _ _; do
     if [ "$strand" == "-" ]; then
-        seqkit grep -w 0 -ip "$id" "$ASSEMBLY" | seqkit seq -r -p -t dna >> ordered_and_oriented_to_${base_name_reference}_assembly.fasta
+        seqkit grep -w 0 -ip "$id" "$ASSEMBLY" | seqkit seq -v -r -p -t dna >> ordered_and_oriented_to_${base_name_reference}_assembly.fasta
     else
         seqkit grep -w 0 -ip "$id" "$ASSEMBLY" >> ordered_and_oriented_to_${base_name_reference}_assembly.fasta
     fi
